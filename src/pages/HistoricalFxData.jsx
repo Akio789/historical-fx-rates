daa2b9d440f06cf6566b9407e8d9867dc4f97e29
Akio Olvera <akiooarao@gmail.com>
2022-05-19 12:27:10 -0600

    Adding c3.js

--- src/pages/HistoricalFxData.jsx
+++ src/pages/HistoricalFxData.jsx
@@ -1,13 +1,27 @@
 import React, { useEffect } from 'react';
-import { fetchHistoricalRates } from '../api/fx_rates';
+import c3 from 'c3';
+import 'c3/c3.css';
+// import { fetchHistoricalRates } from '../api/fx_rates';
 
 const HistoricalFxData = () => {
   useEffect(() => {
-    fetchHistoricalRates('2022-01-01', 'USD', ['EUR', 'CAD']);
+    // fetchHistoricalRates('2022-01-01', 'USD', ['EUR', 'CAD']);
+    c3.generate({
+      bindto: '#historical-fx-data-chart',
+      data: {
+        columns: [
+          ['data1', 30, 200, 100, 400, 150, 250],
+          ['data2', 50, 20, 10, 40, 15, 25],
+        ],
+      },
+    });
   }, []);
 
   return (
-    <div>HistoricalFxData</div>
+    <div>
+      <div>HistoricalFxData</div>
+      <div id="historical-fx-data-chart" />
+    </div>
   );
 };
 
